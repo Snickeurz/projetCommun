f53b01072d41980ee44721728241934ee82d7d46
Snickeurz <nicolassibaud@hotmail.com>
2018-06-20 18:01:11 +0200

    sql

--- projetcommun.sql
+++ projetcommun.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 20 Juin 2018 à 07:41
+-- Généré le :  Mer 20 Juin 2018 à 16:00
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -36,25 +36,6 @@ CREATE TABLE `account` (
   `phone` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `account`
---
-
-INSERT INTO `account` (`id`, `firstname`, `lastname`, `email`, `password`, `role`, `phone`) VALUES
-(1, 'unPrenom', 'unNom', 'what@free.fr', 'password', 1, 859959561),
-(2, 'Josébové', 'Entreprise', 'entreprise', 'entreprise', 0, 632323233),
-(4, 'entreprise2', 'entreprise2', 'entreprise2', 'entreprise2', 0, 0),
-(5, 'client2', 'client2', 'client2', 'client2', 1, 0),
-(7, 'Romain', 'Fleury', 'fleuryromain17@gmail.com', 'rf', 1, 0),
-(8, 'Nestlé', 'entreprise', 'nestlé@gmail.com', 'ent', 0, 0),
-(9, 'Google', 'entreprise', 'google@gmail.com', 'ent2', 0, 0),
-(10, 'Alexandre', 'Romeo', 'alexandre.romeo.descartes@gmail.com', 'ar', 1, 0),
-(11, 'Paris1', 'entreprise', 'paris1@gmail.com', 'paris', 0, 0),
-(12, 'Natixis', 'entreprise', 'natixis@gmail.com', 'natixis', 0, 0),
-(13, 'Nicolas', 'Sibaud', 'nicolassibaud@gmail.com', 'ns', 1, 0),
-(14, 'Nicolas', 'SIBAUD', 'abcdes@hotmail.fr', '123', 1, 0),
-(15, 'test', 'sdfsdf', 'what@free.fr', 'password', 1, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -68,21 +49,9 @@ CREATE TABLE `contrat` (
   `idClient` int(11) DEFAULT NULL,
   `idEntreprise` int(11) DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
-  `dateupload` datetime DEFAULT NULL
+  `dateupload` datetime DEFAULT NULL,
+  `description` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `contrat`
---
-
-INSERT INTO `contrat` (`id`, `contratUrl`, `nomContrat`, `idClient`, `idEntreprise`, `status`, `dateupload`) VALUES
-(1, 'index.fxml', 'unnomdecontrat', 2, 1, 0, '2018-05-12 00:00:00'),
-(3, 'test2', 'test2', 5, 4, 1, '2018-02-09 00:00:00'),
-(4, 'test3', 'test3', 5, 4, 0, '2018-02-09 00:00:00'),
-(6, 'index.fxml', NULL, NULL, NULL, NULL, NULL),
-(7, '.gitconfig', NULL, NULL, NULL, NULL, NULL),
-(9, 'chemin/fichier2.ext', 'Contrat numéro 2', 1, 4, 1, '2017-11-22 00:00:00'),
-(15, './uploads/contrats/Romeo/contrat.txt', 'contrat.txt', 10, 2, 1, '2018-06-18 20:59:06');
 
 -- --------------------------------------------------------
 
@@ -96,15 +65,6 @@ CREATE TABLE `faq` (
   `reponse` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `faq`
---
-
-INSERT INTO `faq` (`id`, `question`, `reponse`) VALUES
-(1, 'Comment contacter les administrateurs du site ? ', 'L\'onglet \'Contact\' est là pour vous aider ! Remplissez le formulaire et nous vous répondrons dès que possible. Si votre question est pertinente pour les autres utilisateurs, elle apparaitra dans notre FAQ.'),
-(2, 'A quoi correspondent les notifications ?', 'Les notifications indiquent qu\'une action a été effectuée.'),
-(3, 'Quelle sorte de contrat puis-je déposer sur le site ?', 'Vous pouvez déposer tous les contrats que vous jugez utiles selon vos besoins.');
-
 -- --------------------------------------------------------
 
 --
@@ -117,19 +77,8 @@ CREATE TABLE `notification` (
   `titre` varchar(40) DEFAULT NULL,
   `message` varchar(100) DEFAULT NULL,
   `lien` varchar(255) DEFAULT NULL,
-  `idEntreprise` varchar(255) DEFAULT NULL,
-  `idClient` int(11) NOT NULL
+  `idAccount` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `notification`
---
-
-INSERT INTO `notification` (`id`, `date`, `titre`, `message`, `lien`, `idEntreprise`, `idClient`) VALUES
-(1, '2018-02-01 12:50:36', 'Signez votre contrat', 'Vous devez signer le contrat', './index.php?uc=redirect&ac=profil', '2', 1),
-(2, '2018-02-05 08:06:44', 'Titre 2', 'Message notification', './index.php?uc=redirect&ac=profil', '2', 1),
-(3, '2018-04-27 09:50:59', 'Fin de contrat', 'Votre contrat prendra fin a la date donnée', 'lienContrat', '2', 1),
-(4, '2017-08-01 16:59:00', 'Voici un titre', 'Voici un message', 'voici_un__lien', '2', 1);
 
 -- --------------------------------------------------------
 
@@ -145,15 +94,6 @@ CREATE TABLE `preferences` (
   `notifMail` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `preferences`
---
-
-INSERT INTO `preferences` (`id`, `idAccount`, `getNotif`, `newsLetter`, `notifMail`) VALUES
-(2, 1, 1, 1, 1),
-(3, 14, 0, 1, 1),
-(4, 15, 0, 0, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -165,14 +105,6 @@ CREATE TABLE `relation_client_entreprise` (
   `idClient` int(11) NOT NULL,
   `idEntreprise` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `relation_client_entreprise`
---
-
-INSERT INTO `relation_client_entreprise` (`id`, `idClient`, `idEntreprise`) VALUES
-(1, 10, 2),
-(2, 14, 2);
 
 --
 -- Index pour les tables exportées
@@ -227,12 +159,12 @@ ALTER TABLE `relation_client_entreprise`
 -- AUTO_INCREMENT pour la table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT pour la table `contrat`
 --
 ALTER TABLE `contrat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT pour la table `faq`
 --
@@ -242,17 +174,17 @@ ALTER TABLE `faq`
 -- AUTO_INCREMENT pour la table `notification`
 --
 ALTER TABLE `notification`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `preferences`
 --
 ALTER TABLE `preferences`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `relation_client_entreprise`
 --
 ALTER TABLE `relation_client_entreprise`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Contraintes pour les tables exportées
 --
